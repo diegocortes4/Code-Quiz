01ba53037b6629e46b3df230bba7fbc2f9d2e89e
Diego Cortes <diegocortes4@gmail.com>
2020-09-29 18:19:23 -0400

    Added Question, function Start

--- app.js
+++ app.js
@@ -2,3 +2,38 @@ var starContainer = document.getElementsByClassName("Container")[0]
 var starButtom = document.getElementById("Start")
 var startQuestion = document.getElementsByClassName("Questions")[0] 
 var HighScore = document.getElementsByClassName("HighScore")[0]
+
+var Questions = [
+{
+   Question: "",
+   Answer:[],
+   CorrectAnswer: ""
+
+} ,{
+    Question: "",
+    Answer:[],
+    CorrectAnswer: ""
+ 
+ } ,{
+    Question: "",
+    Answer:[],
+    CorrectAnswer: ""
+ 
+ } ,{
+    Question: "",
+    Answer:[],
+    CorrectAnswer: ""
+ 
+ } ,{
+    Question: "",
+    Answer:[],
+    CorrectAnswer: ""
+ 
+ } 
+]
+console.log(HighScore)
+
+function Start (){
+   starContainer.setAttribute("class", "hide");
+
+} starButtom.onclick = Start;
