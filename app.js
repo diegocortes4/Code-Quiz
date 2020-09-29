1cf10b1a958d4d26f5e95007d907eb9441674730
Diego Cortes <diegocortes4@gmail.com>
2020-09-29 18:15:12 -0400

    Added Var

---
+++ app.js
@@ -0,0 +1,4 @@
+var starContainer = document.getElementsByClassName("Container")[0]
+var starButtom = document.getElementById("Start")
+var startQuestion = document.getElementsByClassName("Questions")[0] 
+var HighScore = document.getElementsByClassName("HighScore")[0]
