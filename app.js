504ec93c143941438ef542ce9cb2e60355669ad2
Diego Cortes <diegocortes4@gmail.com>
2020-09-30 09:56:47 -0400

    Added the timer

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
+// Var
+
 var starContainer = document.getElementsByClassName("Container")[0]
 var starButtom = document.getElementById("Start")
 var startQuestion = document.getElementsByClassName("Questions")[0] 
 var HighScore = document.getElementsByClassName("HighScore")[0]
+
+
+// Questions
 
 var Questions = [
 {
@@ -37,3 +42,23 @@ function Start (){
    starContainer.setAttribute("class", "hide");
 
 } starButtom.onclick = Start;
+
+// Timer
+var secondsEl = document.querySelector("#seconds");
+var secondsLeft = 200;
+
+function counter(){
+
+
+    var timeLeft = 75;
+    var countDownTimer = setInterval(function () {
+        secondsLeft--;
+        
+       secondsLeft.textContent = "Time" + secondsLeft;
+        timeLeft -= 1;
+        if (timeLeft === 0) {
+            clearInterval(countDownTimer);
+            alert("Your Score is 0")
+        }
+    }, 1000);
+}
